a9ce14fbea0d6d4e900ad601b7d10d575682f064
Le van huyen <92767302+vnhyen1704@users.noreply.github.com>
2023-11-04 21:26:24 +0700

    remove themes not use

--- wp-content/themes/kadence/assets/js/admin/customizer.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'c884a9c57f7007b53b4c');
--- wp-content/themes/kadence/inc/customizer/react/src/textarea/control.js
+++
@@ -1,8 +0,0 @@
-import TextareaComponent from './textarea-component.js';
-
-export const TextareaControl = wp.customize.KadenceControl.extend( {
-	renderContent: function renderContent() {
-		let control = this;
-	ReactDOM.render( <TextareaComponent control={ control } />, control.container[0] );
-	}
-} );
